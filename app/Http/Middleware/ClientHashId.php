196857708199d833ec6c69a6730af93c858cc6d3
Scott Slater <slaterama@gmail.com>
2018-10-19 11:38:40 -0400

    Changes to authenticatinator, also TEMPORARY change to Kernel.php (disabling VerifyCsrfToken)

---
+++ app/Http/Middleware/ClientHashId.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+// use Hashids;
+
+class ClientHashId
+{
+    // TODO See
+    // 'class ClientHashId'
+    // in https://github.com/laravel/passport/issues/14
+
+    // I might need to add https://packagist.org/packages/vinkla/hashids?
+    // i.e. composer require vinkla/hashids
+}
